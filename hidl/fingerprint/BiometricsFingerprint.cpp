64a01ea39df92438cd9428eef9f9b3183ea635a1
S8M1T <sumit.mohanty.chandan2000@gmail.com>
2022-06-12 07:10:04 +0530

    fingerprint: Add blank callbacks for udfps Without this, build will throw error below:
    device/xiaomi/raphael/fingerprint/BiometricsFingerprint.cpp:209:25: error: allocating an object of abstract class type 'android::hardware::biometrics::fingerprint::V2_3::implementation::BiometricsFingerprint'
            sInstance = new BiometricsFingerprint();
                            ^
    out/soong/.intermediates/hardware/interfaces/biometrics/fingerprint/2.3/android.hardware.biometrics.fingerprint@2.3_genc++_headers/gen/android/hardware/biometrics/fingerprint/2.3/IBiometricsFingerprint.h:214:47: note: unimplemented pure virtual method 'onShowUdfpsOverlay' in 'BiometricsFingerprint'
        virtual ::android::hardware::Return<void> onShowUdfpsOverlay() = 0;
                                                  ^
    out/soong/.intermediates/hardware/interfaces/biometrics/fingerprint/2.3/android.hardware.biometrics.fingerprint@2.3_genc++_headers/gen/android/hardware/biometrics/fingerprint/2.3/IBiometricsFingerprint.h:219:47: note: unimplemented pure virtual method 'onHideUdfpsOverlay' in 'BiometricsFingerprint'
        virtual ::android::hardware::Return<void> onHideUdfpsOverlay() = 0;
                                                  ^
    since we don't use it, for now let's just satisfy it with blank callbacks.
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- hidl/fingerprint/BiometricsFingerprint.cpp
+++ hidl/fingerprint/BiometricsFingerprint.cpp
@@ -430,7 +430,15 @@ void BiometricsFingerprint::notify(const fingerprint_msg_t *msg) {
     }
 }
 
-} // namespace implementation
+Return<void> BiometricsFingerprint::onShowUdfpsOverlay() {
+    return Void();
+}
+
+Return<void> BiometricsFingerprint::onHideUdfpsOverlay() {
+    return Void();
+}
+
+}  // namespace implementation
 }  // namespace V2_3
 }  // namespace fingerprint
 }  // namespace biometrics
